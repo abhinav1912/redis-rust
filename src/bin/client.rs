6cef3ba300aec65ff8e1d289170f9af79382c5d5
abhinav1912 <abhinavmathur1912@gmail.com>
2023-04-20 00:17:32 +0530

    refactor: async client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,4 +1,6 @@
-use std::{net::{SocketAddrV4, TcpStream}, io::{Write, Read}, str::FromStr};
+use std::{net::{SocketAddrV4}, io::{Write, Read}, str::FromStr};
+
+use tokio::{net::TcpStream, io::{AsyncWriteExt, AsyncReadExt}};
 
 const MESSAGE_LENGTH: usize = 64;
 
@@ -11,23 +13,23 @@ impl Client {
         Client { server_address }
     }
 
-    pub fn connect_to_server(&self) {
-        let mut stream = TcpStream::connect(self.server_address).expect("Can't connect to server");
+    pub async fn connect_to_server(&self) {
+        let mut stream = TcpStream::connect(self.server_address).await.expect("Can't connect to server");
         let write_buf = b"Hello Server!";
-        stream.write(write_buf);
-        stream.flush();
+        stream.write(write_buf).await;
+        stream.flush().await;
 
         let mut read_buf = [0u8; MESSAGE_LENGTH];
-        stream.read(&mut read_buf);
+        stream.read(&mut read_buf).await;
 
         let server_response = std::str::from_utf8(&read_buf).expect("Invalid UTF8 in server response");
         println!("Server said: {}", server_response);
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let server_address = SocketAddrV4::from_str("127.0.0.1:8000").expect("Unable to create server address");
     let client_one = Client::new(server_address);
-    client_one.connect_to_server();
-    println!("Hello, world!");
+    client_one.connect_to_server().await;
 }
